4079617834ceceb5e32efee69326626db2820992
Yicheng Chen <yicheng-chen@outlook.com>
2023-07-08 21:01:36 +0800

    reopened odom2base_tf

--- src/planner/scripts/odom2base_tf.py
+++ src/planner/scripts/odom2base_tf.py
@@ -1,6 +1,6 @@
 '''
 Author: Yicheng Chen (yicheng-chen@outlook.com)
-LastEditTime: 2023-07-08 11:41:44
+LastEditTime: 2023-07-08 20:59:34
 '''
 
 import rospy
@@ -14,6 +14,10 @@ class TFServer():
         # Node
         rospy.init_node(node_name, anonymous=False)
 
+        self.tfs = TransformStamped()
+        self.tfs.header.frame_id = "odom"
+        self.tfs.child_frame_id = "base_link"
+
         # Subscribers
         self.odom_sub = rospy.Subscriber('mavros/local_position/odom', Odometry, self.odom_cb)
 
@@ -21,17 +25,15 @@ class TFServer():
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
 
     def odom_cb(self, odom):
-        tfs = TransformStamped()
-        tfs.header.stamp = rospy.Time.now()
-        tfs.header.frame_id = "odom"
-        tfs.child_frame_id = "base_link"
+        self.tfs.header.stamp = rospy.Time.now()
 
-        tfs.transform.translation.x = odom.pose.pose.position.x
-        tfs.transform.translation.y = odom.pose.pose.position.y
-        tfs.transform.translation.z = odom.pose.pose.position.z
+        self.tfs.transform.translation.x = odom.pose.pose.position.x
+        self.tfs.transform.translation.y = odom.pose.pose.position.y
+        self.tfs.transform.translation.z = odom.pose.pose.position.z
 
-        tfs.transform.rotation = odom.pose.pose.orientation
-        self.tf_broadcaster.sendTransform(tfs)
+        self.tfs.transform.rotation = odom.pose.pose.orientation
+
+        self.tf_broadcaster.sendTransform(self.tfs)
 
 
 if __name__ == "__main__":
