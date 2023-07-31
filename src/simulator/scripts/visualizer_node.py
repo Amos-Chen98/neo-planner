26f76e3ad521c65383707fac05e904de89f00e50
Yicheng Chen <yicheng-chen@outlook.com>
2023-07-31 16:01:27 +0800

    updated visualizer_node

--- src/simulator/scripts/visualizer_node.py
+++ src/simulator/scripts/visualizer_node.py
@@ -1,6 +1,6 @@
 '''
 Author: Yicheng Chen (yicheng-chen@outlook.com)
-LastEditTime: 2023-07-15 17:17:46
+LastEditTime: 2023-07-31 15:36:27
 '''
 from nav_msgs.msg import Path
 import rospy
@@ -17,13 +17,26 @@ class Visualizer():
         self.real_path = Path()
         self.real_path.header.frame_id = "map"
 
+        # drone marker
+        self.drone_marker = Marker()
+        self.drone_marker.header.frame_id = "map"
+        self.drone_marker.type = self.drone_marker.MESH_RESOURCE
+        self.drone_marker.mesh_resource = "package://simulator/models/meshes/iris.stl"
+        self.drone_marker.color.r = 81/256
+        self.drone_marker.color.g = 196/256
+        self.drone_marker.color.b = 211/256
+        self.drone_marker.color.a = 1
+        self.drone_marker.scale.x = 1.5
+        self.drone_marker.scale.y = 1.5
+        self.drone_marker.scale.z = 1.5
+
         # Subscribers
         self.odom_sub = rospy.Subscriber('/mavros/local_position/odom', Odometry, self.odom_cb)
 
         # Publishers
         self.marker_pub = rospy.Publisher('/robotMarker', MarkerArray, queue_size=10)
         self.real_path_pub = rospy.Publisher('/real_path', Path, queue_size=10)
-        # self.drone_marker_pub = rospy.Publisher('/drone_marker', Marker, queue_size=10)
+        self.drone_marker_pub = rospy.Publisher('/drone_marker', Marker, queue_size=10)
 
     def odom_cb(self, data):
         pose_stamped = PoseStamped()
@@ -31,19 +44,8 @@ class Visualizer():
         self.real_path.poses.append(pose_stamped)
         self.real_path_pub.publish(self.real_path)
 
-        # drone_marker = Marker()
-        # drone_marker.header.frame_id = "map"
-        # drone_marker.pose = data.pose.pose
-        # drone_marker.type = drone_marker.MESH_RESOURCE
-        # drone_marker.mesh_resource = "package://simulator/models/meshes/iris.stl"
-        # drone_marker.color.r = 81/256
-        # drone_marker.color.g = 196/256
-        # drone_marker.color.b = 211/256
-        # drone_marker.color.a = 1
-        # drone_marker.scale.x = 1
-        # drone_marker.scale.y = 1
-        # drone_marker.scale.z = 1
-        # self.drone_marker_pub.publish(drone_marker)
+        self.drone_marker.pose = data.pose.pose
+        self.drone_marker_pub.publish(self.drone_marker)
 
 
 if __name__ == "__main__":
