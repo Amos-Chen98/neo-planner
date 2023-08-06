6f37bb4403bd985018c0bacced26c8740e9adc00
Yicheng Chen <yicheng-chen@outlook.com>
2023-08-06 22:01:27 +0800

    added onnx2trt

---
+++ src/planner/scripts/onnx2trt.py
@@ -0,0 +1,62 @@
+'''
+Author: Yicheng Chen (yicheng-chen@outlook.com)
+LastEditTime: 2023-08-06 21:15:19
+'''
+import os
+import torch
+import onnx
+import tensorrt as trt
+
+IMG_WIDTH = 480
+IMG_HEIGHT = 360
+VECTOR_SIZE = 24
+INPUT_SIZE = IMG_WIDTH * IMG_HEIGHT + VECTOR_SIZE
+EXPLICIT_BATCH = 1
+
+current_path = os.path.dirname(os.path.abspath(__file__))[:-8]  # -7 remove '/scripts'
+onnx_save_path = '/saved_net/planner_net.onnx'
+trt_save_path = '/saved_net/planner_net.trt'
+
+
+def convert_onnx_to_trt(onnx_model_path, trt_model_path):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("Device: ", device)
+    print("TensorRT version: ", trt.__version__)
+
+    # Load onnx model
+    onnx_model = onnx.load(onnx_model_path)
+    print("Onnx model loaded!")
+
+    # Create builder and network
+    logger = trt.Logger(trt.Logger.ERROR)
+    builder = trt.Builder(logger)
+    network = builder.create_network(EXPLICIT_BATCH)
+
+    # Create ONNX parser
+    parser = trt.OnnxParser(network, logger)
+
+    if not parser.parse(onnx_model.SerializeToString()):
+        print("ERROR: Failed to parse the ONNX file.")
+        for error in range(parser.num_errors):
+            print(parser.get_error(error))
+        exit(0)
+
+    # Set builder config and profile
+    config = builder.create_builder_config()
+    profile = builder.create_optimization_profile()
+    profile.set_shape("input", (1, INPUT_SIZE), (1, INPUT_SIZE), (1, INPUT_SIZE))
+    config.add_optimization_profile(profile)
+
+    # build and save serialized network
+    model_in_memory = builder.build_serialized_network(network, config)
+    with open(trt_model_path, "wb") as f:
+        f.write(model_in_memory)
+    print("TensorRT model saved!")
+
+
+if __name__ == "__main__":
+
+    onnx_model_path = current_path + onnx_save_path
+    trt_model_path = current_path + trt_save_path
+
+    convert_onnx_to_trt(onnx_model_path, trt_model_path)
