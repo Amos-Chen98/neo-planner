c74b5cec55217af32244fc001b63c6ed80e9b763
Yicheng Chen <yicheng-chen@outlook.com>
2023-08-13 17:29:11 +0800

    minor

--- sim.sh
+++ sim.sh
@@ -3,6 +3,6 @@ sleep 3
 gnome-terminal --tab --title="map_server" --command="bash -c 'roslaunch planner map_server_onboard.launch; exec bash'" &
 gnome-terminal --tab --title="planner" --command="bash -c 'roslaunch planner planner.launch; exec bash'" &
 gnome-terminal --tab --title="manager" --command="bash -c 'roslaunch planner manager.launch; exec bash'" &
-sleep 15
+sleep 17
 rostopic pub /move_base_simple/goal geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: 30.0, y: 0.0, z: 0.0}, orientation: {w: 1.0}}}' &
 wait
