a74c9d08bd42282bb42c03e995fcd498ca08ff9f
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2024-03-03 11:30:50 +0900

    test several random generated worlds

--- src/planner/scripts/bash/multi_run.sh
+++ src/planner/scripts/bash/multi_run.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+is_save_rosbag=${2:-"false"}  # Default to true if no argument is provided
+echo "The is_save_rosbag is $is_save_rosbag"
 
-echo "++++++++++ run ./scripts/bash/demo_auto_stop.sh poles ++++++++++"
-./scripts/bash/demo_auto_stop.sh poles
+echo "++++++++++ Execution stated. ++++++++++"
+./scripts/bash/demo_auto_stop.sh "rand_world_10" "$is_save_rosbag"
 echo "++++++++++ Execution finished. ++++++++++"
 
 sleep 10
 
-echo "++++++++++ run ./scripts/bash/demo_auto_stop.sh bricks ++++++++++"
-./scripts/bash/demo_auto_stop.sh bricks
+echo "++++++++++ Execution stated. ++++++++++"
+./scripts/bash/demo_auto_stop.sh "rand_world_20" "$is_save_rosbag"
 echo "++++++++++ Execution finished. ++++++++++"
 
+sleep 10
+
+echo "++++++++++ Execution stated. ++++++++++"
+./scripts/bash/demo_auto_stop.sh "rand_world_40" "$is_save_rosbag"
+echo "++++++++++ Execution finished. ++++++++++"
 
 ## Check if the number of repetitions is provided as an argument
 #if [ -z "$1" ]; then
