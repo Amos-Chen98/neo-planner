d3c558f809536f709937adc5c33917deab944751
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2024-03-04 10:47:38 +0900

    refactor multi_run.sh, wrap function

--- src/planner/scripts/bash/multi_run.sh
+++ src/planner/scripts/bash/multi_run.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
+num_repeats=${1:-"1"}  # Default to 1 if no argument is provided
+echo "The number of repeats for each experiments is $num_repeats"
+
 is_save_rosbag=${2:-"false"}  # Default to true if no argument is provided
 echo "The is_save_rosbag is $is_save_rosbag"
 
 # selected_planner: 'basic', 'batch', 'record', 'nn', 'enhanced', or 'warmstart'
 # replan_mode: 'global', 'online', or 'periodic'
 
-echo "++++++++++ Execution stated. ++++++++++"
-./scripts/bash/demo_auto_stop.sh "nn" "rand_world_10" "periodic" "$is_save_rosbag"
-echo "++++++++++ Execution finished. ++++++++++"
+min_test_unit() {
+  local num_runs=$1
+  local world=$2
 
-sleep 10
+  # Loop to execute the script the specified number of times
+  for (( i = 1; i <= num_runs; i++ ))
+  do
+    echo "========== Execution $i run  =========="
 
-echo "++++++++++ Execution stated. ++++++++++"
-./scripts/bash/demo_auto_stop.sh "basic" "rand_world_20" "periodic" "$is_save_rosbag"
-echo "++++++++++ Execution finished. ++++++++++"
+    echo "++++++++++ Execution started. ++++++++++"
+    ./scripts/bash/demo_auto_stop.sh "nn" "$world" "periodic" "$is_save_rosbag"
+    echo "++++++++++ Execution finished. ++++++++++"
+    sleep 10
 
-sleep 10
+    echo "++++++++++ Execution started. ++++++++++"
+    ./scripts/bash/demo_auto_stop.sh "basic" "$world" "periodic" "$is_save_rosbag"
+    echo "++++++++++ Execution finished. ++++++++++"
+    sleep 10
 
-echo "++++++++++ Execution stated. ++++++++++"
-./scripts/bash/demo_auto_stop.sh "enhanced" "rand_world_40" "periodic" "$is_save_rosbag"
-echo "++++++++++ Execution finished. ++++++++++"
+    echo "++++++++++ Execution started. ++++++++++"
+    ./scripts/bash/demo_auto_stop.sh "enhanced" "$world" "periodic" "$is_save_rosbag"
+    echo "++++++++++ Execution finished. ++++++++++"
+    sleep 10
+  done
+}
 
-## Check if the number of repetitions is provided as an argument
-#if [ -z "$1" ]; then
-#  echo "Please provide the number of times you want to repeat the script."
-#  echo "Usage: $0 <number-of-times>"
-#  exit 1
-#fi
-#
-## Extract the number of repetitions
-#num_repeats=$1
-#
-## Loop to execute the script the specified number of times
-#for (( i = 1; i <= num_repeats; i++ ))
-#do
-#  echo "Execution $i of ./scripts/bash/demo_auto_stop.sh bricks"
-#  ./scripts/bash/demo_auto_stop.sh bricks
-#  echo "Execution $i finished."
-#done
+# change worlds from here
+min_test_unit $num_repeats "rand_world_10"
+min_test_unit $num_repeats "rand_world_20"
+min_test_unit $num_repeats "rand_world_40"
