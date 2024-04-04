461bf9a7af9ab7466b2eaf7a052056955cb26286
Yicheng Chen <yicheng-chen@outlook.com>
2024-04-04 16:26:24 +0800

    Retain three significant digits in analyze_data.py

--- src/planner/scripts/bash/analyze_data.py
+++ src/planner/scripts/bash/analyze_data.py
@@ -1,10 +1,10 @@
 '''
 created by Jinjie LI, 2024/03/05
 '''
+
+
 import os
 import rospkg
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -14,52 +14,58 @@ def analyze_with_planner(in_data, world):
         # filter the data
         filtered_data = in_data[in_data['planner'] == planner]
 
+        success_cases = filtered_data[filtered_data['if_reached_target'] == 1]
+
         # print success rate
-        success_rate = filtered_data['if_reached_target'].sum() / len(filtered_data)
+        success_rate = len(success_cases) / len(filtered_data)
         print(planner + ' in ' + world + ' success rate: ' + str(success_rate))
 
-        filtered_data = filtered_data[filtered_data['if_reached_target'] == 1]  # only consider the successful cases
-
         # print average weighted metric
-        average_weighted_metric = filtered_data['weighted_metric'].mean()
+        average_weighted_metric = success_cases['weighted_metric'].mean()
+        average_weighted_metric = '%.3g' % average_weighted_metric
         print(planner + ' in ' + world + ' average weighted metric: ' + str(average_weighted_metric))
 
         # print average planning duration
-        average_planning_duration = filtered_data['average_planning_duration'].mean()
+        average_planning_duration = success_cases['average_planning_duration'].mean()
+        average_planning_duration = '%.3g' % average_planning_duration
         print(planner + ' in ' + world + ' average planning duration: ' + str(average_planning_duration))
 
         # print average iteration number
-        average_iter_num = filtered_data['average_iter_num'].mean()
+        average_iter_num = success_cases['average_iter_num'].mean()
+        average_iter_num = '%.3g' % average_iter_num
         print(planner + ' in ' + world + ' average iteration number: ' + str(average_iter_num))
 
         # print average target find time
-        average_target_find_time = filtered_data['target_find_time'].mean()
+        average_target_find_time = success_cases['target_find_time'].mean()
+        average_target_find_time = '%.3g' % average_target_find_time
         print(planner + ' in ' + world + ' average target find time: ' + str(average_target_find_time))
 
         # print average total planning times
-        average_total_planning_times = filtered_data['total_planning_times'].mean()
+        average_total_planning_times = success_cases['total_planning_times'].mean()
+        average_total_planning_times = '%.3g' % average_total_planning_times
         print(planner + ' in ' + world + ' average total planning times: ' + str(average_total_planning_times) + '\n')
 
 
-rospack = rospkg.RosPack()
-param_path = os.path.join(rospack.get_path("planner"), "data", "planning_metrics.txt")
+if __name__ == '__main__':
+    rospack = rospkg.RosPack()
+    param_path = os.path.join(rospack.get_path("planner"), "data", "planning_metrics.txt")
 
-# read data
-data = pd.read_csv(param_path, sep=' ', header=None)
-data.columns = ['date', 'time', 'world', 'world_num_model', 'planner', 'replan_mode', 'if_reached_target', 'target_x',
-                'target_y',
-                'target_find_time', 'max_target_find_time', 'weighted_metric', 'average_iter_num',
-                'average_planning_duration', 'total_planning_times']
+    # read data
+    data = pd.read_csv(param_path, sep=' ', header=None)
+    data.columns = ['date', 'time', 'world', 'world_num_model', 'planner', 'replan_mode', 'if_reached_target', 'target_x',
+                    'target_y',
+                    'target_find_time', 'max_target_find_time', 'weighted_metric', 'average_iter_num',
+                    'average_planning_duration', 'total_planning_times']
 
-multi_num_models = data['world_num_model'].unique()
+    multi_num_models = data['world_num_model'].unique()
 
-for num_model in multi_num_models:
-    data_num_model = data[data['world_num_model'] == num_model]
-    print('For ' + str(num_model) + ' models:')
-    if num_model == 0:
-        worlds = data_num_model['world'].unique()
-        for world in worlds:
-            data_filtered = data_num_model[data_num_model['world'] == world]
-            analyze_with_planner(data_filtered, world)
-    else:
-        analyze_with_planner(data_num_model, str(num_model))
+    for num_model in multi_num_models:
+        data_num_model = data[data['world_num_model'] == num_model]
+        print('For ' + str(num_model) + ' models:')
+        if num_model == 0:
+            worlds = data_num_model['world'].unique()
+            for world in worlds:
+                data_filtered = data_num_model[data_num_model['world'] == world]
+                analyze_with_planner(data_filtered, world)
+        else:
+            analyze_with_planner(data_num_model, str(num_model))
